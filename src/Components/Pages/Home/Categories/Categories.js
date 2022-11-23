fceb367cd454fd33f97ff6f9097fc63b9ee244ad
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-11-24 02:23:34 +0600

    banner created

--- src/Components/Pages/Home/Categories/Categories.js
+++ src/Components/Pages/Home/Categories/Categories.js
@@ -2,31 +2,45 @@ import React from "react";
 import cruiser from "../../../../assets/cruiser-logo.jpg";
 import sport from "../../../../assets/sport-logo.png";
 import scooter from "../../../../assets/scooter-logo.png";
+import CategoryCard from "./CategoryCard";
 
 const Categories = () => {
   const categories = [
     {
       id: 1,
       name: "Cruiser",
-      image: cruiser
+      image: cruiser,
     },
     {
       id: 2,
       name: "Sport",
-      image: sport
+      image: sport,
     },
     {
       id: 3,
       name: "Scooter",
-      image: scooter
+      image: scooter,
     },
   ];
 
-  return <div>
-    {
-        categories.map(category=>)
-    }
-  </div>;
+  return (
+    <div>
+      <div className="my-14">
+        <h2 className="text-5xl text-red-600 font-semibold text-center mb-4">
+          Categories
+        </h2>
+        <h3 className="text-xl text-center">
+          We have multiple items under each category. Click on products and
+          check those out.
+        </h3>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 m-10">
+        {categories.map((category) => (
+          <CategoryCard key={category.id} category={category}></CategoryCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
--- src/Components/Pages/Home/Categories/CategoryCard.js
+++ src/Components/Pages/Home/Categories/CategoryCard.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const CategoryCard = () => {
-  return <div></div>;
+const CategoryCard = ({ category }) => {
+  return (
+    <div>
+      <div className="card shadow-xl">
+        <figure className=" pt-10">
+          <img src={category.image} alt="" className="rounded-xl h-14" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{category.name}</h2>
+          <div className="card-actions">
+            <button className="btn btn-ghost btn-sm">Products</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryCard;
